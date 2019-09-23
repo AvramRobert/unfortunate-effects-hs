a08d9084bf8a16245a79545f6ec64d715768f96d
Robert M. Avram <robert.avram@rewe-digital.com>
2019-09-23 23:34:49 +0200

    Add logging

--- src/Effects/Horizontal/EffStaticExample.hs
+++ src/Effects/Horizontal/EffStaticExample.hs
@@ -3,7 +3,8 @@
 module Effects.Horizontal.EffStaticExample where
 
 import Effects.Horizontal.Eff
-import Effects.Horizontal.Static.Union2
+import qualified Effects.Horizontal.Static.Union2 as U2
+import qualified Effects.Horizontal.Static.Union3 as U3
 import Effects.Future
 import Effects.Writer
 import System.IO.Unsafe (unsafePerformIO)
@@ -30,24 +31,38 @@ encrypt :: Maybe Entry -> Comp Future (Maybe String)
 encrypt (Just entry) = send (Async $ return $ Just $ hash entry)
 encrypt (Nothing) = send (Sync $ Nothing)
 
-log :: String -> Comp (Writer [String]) ()
-log msg = send (tell [msg])
+echo :: String -> Comp (Writer [String]) ()
+echo msg = send (tell [msg])
 
-runGet :: Comp (Union IO r2) a -> Comp r2 a
-runGet (Value a)          = Value a
-runGet (Effect (P1 io) f) = runGet $ f (unsafePerformIO io)
-runGet (Effect (P2 r) f)  = Effect r (\x -> runGet $ f x)
+decomp :: Comp (U3.Union r1 r2 r3) a -> Comp (U2.Union r2 r3) a
+decomp (Value a)            = Value a
+decomp (Effect (U3.P2 r) f) = Effect (U2.P1 r) (\x -> decomp $ f x) 
+decomp (Effect (U3.P3 r) f) = Effect (U2.P2 r) (\x -> decomp $ f x)
 
-runEncrypt :: Comp Future a -> a
-runEncrypt (Value a)             = a
-runEncrypt (Effect (Async io) f) = runEncrypt $ f $ unsafePerformIO io
-runEncrypt (Effect (Sync a) f)   = runEncrypt $ f a
+runGet :: Comp (U3.Union IO r2 r3) a -> Comp (U2.Union r2 r3) a
+runGet (Effect (U3.P1 io) f) = decomp $ f (unsafePerformIO io)
+runGet computation           = decomp computation
 
-readEncrypt :: String -> Comp (Union IO Future) (Maybe String)
+runEncrypt :: Comp (U2.Union Future r2) a -> Comp r2 a
+runEncrypt (Value a)                     = (Value a)
+runEncrypt (Effect (U2.P1 (Async io)) f) = runEncrypt $ f $ unsafePerformIO io
+runEncrypt (Effect (U2.P1 (Sync a)) f)   = runEncrypt $ f a
+runEncrypt (Effect (U2.P2 r) f)          = Effect r (\x -> runEncrypt $ f x)
+
+runLog :: Comp (Writer [String]) a -> ([String], a)
+runLog (Value a)                  = ([], a)
+runLog (Effect (Writer logs a) f) = let (rest, x) = runLog (f a)
+                                    in (logs <> rest, x)
+
+readEncrypt :: String -> Comp (U3.Union IO Future (Writer [String])) (Maybe String)
 readEncrypt id = do
-    mentry    <- injectP1 $ get id
-    encrypted <- injectP2 $ encrypt mentry
+    mentry    <- U3.injectP1 $ get id
+    _         <- U3.injectP3 $ echo ("Entry is " <> (show mentry))
+    encrypted <- U3.injectP2 $ encrypt mentry
+    _         <- U3.injectP3 $ echo ("Encryped entry")
+    _         <- U3.injectP3 $ echo ("Value is: " <> (show encrypted))
     return encrypted
 
 main :: IO ()
-main = putStrLn $ show $ runEncrypt $ runGet $ readEncrypt "a"
+main = let (logs, _) = runLog $ runEncrypt $ runGet $ readEncrypt "a"
+       in putStrLn $ (unlines logs)
