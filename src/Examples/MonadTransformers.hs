cd11da052d66e3a53a9e824a97c7b686a25d8c87
Robert M. Avram <robert.avram@rewe-digital.com>
2019-09-24 21:22:07 +0200

    Correct transformers example

--- src/Examples/MonadTransformers.hs
+++ src/Examples/MonadTransformers.hs
@@ -12,14 +12,11 @@ import Data.List (find, deleteBy)
 
 newtype Error = Error String deriving (Show)
 
-type DBOp a = MaybeT (FutureT (WriterT [String] IO)) a
+type DBOp a = (FutureT (WriterT [String] IO)) a
 
 data Entry = Entry { index :: String, version :: Integer, payload :: Int } deriving (Show)
 
 dbRef = unsafePerformIO $ newIORef [Entry { index = "a", version = 1, payload = 2 }]
-
-io :: a -> IO a
-io = return
 
 hash :: Entry -> String
 hash (Entry id version payload) = id <> "::" <> (show version) <> "::" <> (show payload)
@@ -37,24 +34,24 @@ echoComp :: String -> Writer [String] ()
 echoComp msg = tell [msg]
 
 -- You have to think idiotocally recursivelly in reverse
+-- Depending on where and how you do the effect, you have to patch the sctructure in very odd ways in order to make it work
 
-get :: String -> DBOp Entry
-get id = MaybeT $ (lift $ lift $ getComp id)
+get :: String -> DBOp (Maybe Entry)
+get id = FutureT $ lift $ fmap pure $ getComp id
 
-encrypt :: Entry -> DBOp String
-encrypt entry = MaybeT $ FutureT $ pure $ runMaybeT $ lift $ encryptComp entry
+encrypt :: Maybe Entry -> DBOp (Maybe String)
+encrypt (Just entry) = FutureT $ pure $ fmap Just $ encryptComp entry
+encrypt (Nothing)    = FutureT $ pure $ pure Nothing
 
 echo :: String -> DBOp ()
-echo msg = MaybeT $ FutureT $ WriterT $ pure $ runFutureT $ runMaybeT $ lift $ lift $ echoComp msg
+echo msg = FutureT $ WriterT $ pure $ fmap pure $ echoComp msg
 
-run :: DBOp a -> ([String], Maybe a)
-run op = case (unsafePerformIO $ runWriterT $ runFutureT $ runMaybeT op) of
-                (Writer logs (Async io)) -> case (unsafePerformIO io) of
-                                                        (Just a)  -> (logs, Just a)
-                                                        (Nothing) -> (logs, Nothing)
+run :: DBOp a -> ([String], a)
+run op = case (unsafePerformIO $ runWriterT $ runFutureT op) of
+                (Writer logs (Async io)) -> (logs, unsafePerformIO io)
                 (Writer logs (Sync a))   -> (logs, a) 
 
-readEncrypt :: String -> DBOp String
+readEncrypt :: String -> DBOp (Maybe String)
 readEncrypt id = do
     mentry    <- get id
     _         <- echo ("Entry is " <> (show mentry))
